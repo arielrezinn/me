1813d63ceac0ee5e19af5814a1c0ef1413a3fc62
arielrezinn <arielrezin@gmail.com>
2020-06-15 15:11:08 -0500

    readme and env deployment troubleshooting

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -18,23 +18,29 @@ export default class About extends Component {
                 <div className="col-md-12 heading-section ftco-animate fadeInUp ftco-animated">
                   <h1 className="big">About</h1>
                   <h2 className="mb-4">About Me</h2>
-                  <p>A small river named Duden flows by their place and supplies it with the necessary regelialia.</p>
+                  <p>
+                  Hello! I'm a software developer with a passion for helping others, and my ultimate goal is
+                  for my work to have a positive impact on people's everyday lives. I'm passionate about
+                  designing accessible software and spreading awareness of topics I'm passionate about.
+                  </p>
+                  <p>
+                  I'm currently in pursuit of my B.A. in Computer Science at the University of Wisconsin-Madison.
+                  Keep reading for more information on my experience and accomplishments!
+                  </p>
                   <ul className="about-info mt-4 px-md-0 px-2">
-                    <li className="d-flex"><span>Name:</span> <span>Ronaldo Fredrickson</span></li>
-                    <li className="d-flex"><span>Date of birth:</span> <span>November 28, 1989</span></li>
-                    <li className="d-flex"><span>Address:</span> <span>San Francisco CA 97987 USA</span></li>
-                    <li className="d-flex"><span>Zip code:</span> <span>1000</span></li>
-                    <li className="d-flex"><span>Email:</span> <span>ronaldo@gmail.com</span></li>
-                    <li className="d-flex"><span>Phone: </span> <span>+1-2234-5678-9-0</span></li>
+                    <li className="d-flex"><span>Name</span> <span>Ariel Rezin</span></li>
+                    <li className="d-flex"><span>Location</span> <span>Madison, Wisconsin</span></li>
+                    <li className="d-flex"><span>Email</span> <span>arielrezin (at) gmail.com</span></li>
                   </ul>
                 </div>
               </div>
               <div className="counter-wrap ftco-animate d-flex mt-md-3 fadeInUp ftco-animated">
                 <div className="text">
-                  <p className="mb-4">
+                  {/* <p className="mb-4">
+                    this snippet is currently not relevant for my purposes but could be used elsewhere in the future
                     <span className="number" data-number={120}>120 </span>
                     <span>Project complete</span>
-                  </p>
+                  </p> */}
                   <p><a href="#" className="btn btn-primary py-3 px-3">Download CV</a></p>
                 </div>
               </div>
